0a3f1018e3c85a5ee137e93375ae5c9a5de631dd
Arshi Siddiqua <arshisiddiqua02@gmail.com>
2026-07-31 22:29:33 +0600

    PYTHON

---
+++ new.py
@@ -0,0 +1,9 @@
+light = input("Enter the light color: ")
+if (light=="red"):
+    print("Stop")
+elif(light=="yellow"):
+    print("Wait")
+elif(light=="green"):
+    print("go")
+else:
+    print("Invalid color")
