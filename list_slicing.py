947530e567a049cb489276cc562232511450b7b5
Arshi Siddiqua <arshisiddiqua02@gmail.com>
2026-08-07 19:45:09 +0600

    PYTHON

---
+++ list_slicing.py
@@ -0,0 +1,5 @@
+marks=[2,2,4,45,6,322,45,3]
+print(marks[2: 5])
+print(marks[2:5:2]) #Only prints the 2nd and 4th index
+print(marks[:3]) #Prints only first 3 index
+print(marks[3:]) #Prints from the 3rd index till end
