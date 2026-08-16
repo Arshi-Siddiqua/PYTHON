0afdb15cabed6f46427dc5a19031616fb7bb5317
Arshi Siddiqua <arshisiddiqua2@gmail.com>
2026-08-16 19:41:45 +0600

    PYTHON

---
+++ array.py
@@ -0,0 +1,12 @@
+car1= "BMW"
+car2= "Porsche"
+car3= "Bently Bentega"
+car4= "Lamborghini"
+car5 = "Ferrari"
+car6= "Rolls Royce"
+car7= "Masratei"
+
+cars=[car1, car2, car3, car4, car5, car6, car7]
+print (cars)
+cars.append("Audi")
+print (cars)
