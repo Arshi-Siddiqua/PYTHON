6dc0d1d218bae29123c4d8d9f85ade8e3415c780
Arshi Siddiqua <arshisiddiqua2@gmail.com>
2026-09-08 16:23:07 +0600

    PYTHON

---
+++ set_practice.py
@@ -0,0 +1,5 @@
+#Question: You are given a list of subjects for students. Assume one classroom is required for 1 subject. How many classrooms are needed by all students?
+
+
+set= {"Python", "Java", "C++", "Python", "JavaScript", "Java", "Python", "Java", "C++", "C"}
+print (len(set))
